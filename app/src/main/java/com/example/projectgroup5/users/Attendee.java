bda8346a97be534e446eed16d32b97a06baa574f
Beryshadow <beryshadow@gmail.com>
2024-10-08 16:38:10 -0400

    bit of refactoring and user class

--- app/src/main/java/com/example/projectgroup5/users/Attendee.java
+++ app/src/main/java/com/example/projectgroup5/users/Attendee.java
@@ -2,11 +2,9 @@ package com.example.projectgroup5.users;
 
 import android.location.Address;
 
-public class Attendee {
-    String userFirstName;
-    String userLastName;
-    String userEmail;
-    String userPasswordHash;
-    String userPhoneNumber;
-    Address userAddress;
+public class Attendee extends User {
+    @Override
+    void SaveLoginInfo() {
+// TODO complete this
+    }
 }
