9cbfce7be9ac274b0204dec7b04a7807d234320f
Ryan B <beryshadow@gmail.com>
2024-11-03 09:17:39 -0500

    added the base of event

--- app/src/main/java/com/example/projectgroup5/events/Event.java
+++ app/src/main/java/com/example/projectgroup5/events/Event.java
@@ -1,6 +1,9 @@
 package com.example.projectgroup5.events;
 
+import com.example.projectgroup5.users.User;
 import com.google.firebase.Timestamp;
+
+import java.util.List;
 
 public class Event {
     // An event has a date, start time, end time, a title, and an address.
@@ -13,6 +16,29 @@ public class Event {
     private Timestamp startTime;
     private Timestamp endTime;
     private Timestamp date;
+    private List<User> participants;
+
+    /**
+     * This constructor is used to create a new event.
+     * It should not be called directly.
+     * Instead use the {@link EventOption#newEvent(String, String, Timestamp, Timestamp, Timestamp, List<User>)}
+     * <p> or {@link EventOption#oldEvent(String, String, Timestamp, Timestamp, Timestamp)}</p>
+     * @param title
+     * @param address
+     * @param startTime
+     * @param endTime
+     * @param date
+     * @param participants
+     */
+    protected Event(String title, String address, Timestamp startTime, Timestamp endTime, Timestamp date, List<User> participants) {
+        this.title = title;
+        this.address = address;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.date = date;
+        this.participants = participants;
+    }
+
 
 
 
---
+++ app/src/main/java/com/example/projectgroup5/events/EventError.java
@@ -0,0 +1,16 @@
+package com.example.projectgroup5.events;
+
+public enum EventError {
+    // enum to define the different errors that can occur when creating an event
+    START_TIME_PAST_ERROR,
+    END_TIME_PAST_ERROR,
+    END_TIME_BEFORE_START_TIME_ERROR,
+    ADDRESS_EMPTY_ERROR,
+    TITLE_EMPTY_ERROR,
+    DATE_EMPTY_ERROR,
+    START_TIME_EMPTY_ERROR,
+    END_TIME_EMPTY_ERROR,
+}
+
+
+
